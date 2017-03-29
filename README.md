97a4a3f3d727594156420ac3f8286b0e312cb167
iamericmaxwell <iamericmaxwell@gmail.com>
2017-03-29 16:07:08 -0700

    Add generated cookbook content

---
+++ README.md
@@ -0,0 +1,4 @@
+# chef_python
+
+TODO: Enter the cookbook description here.
+
---
+++ recipes/default.rb
@@ -0,0 +1,5 @@
+#
+# Cookbook:: chef_python
+# Recipe:: default
+#
+# Copyright:: 2017, The Authors, All Rights Reserved.
